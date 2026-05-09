2b715a1c09d510c922b7c2fa7d3661be11f89164
Leandro Sarracini <lsarraci@c1r6p3.42sp.org.br>
2026-05-09 18:10:49 -0300

    modify: remoção de arquivos desnecessários, atualização de documentação

--- src/engine/vision_ray.c
+++ src/engine/vision_ray.c
@@ -6,12 +6,18 @@
 /*   By: lsarraci <lsarraci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 18:14:07 by lsarraci          #+#    #+#             */
-/*   Updated: 2026/05/01 17:15:46 by lsarraci         ###   ########.fr       */
+/*   Updated: 2026/05/09 18:08:48 by lsarraci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub.h"
 
+/**
+ * auxiliary function to rotate a vector by a given angle
+ * @param v The input vector to rotate
+ * @param a The angle to rotate by (in radians)
+ * @param out The output vector after rotation
+ */
 static void	rotate_ray(t_dcoord v, double a, t_dcoord *out)
 {
 	double	cos_a;
@@ -23,6 +29,16 @@ static void	rotate_ray(t_dcoord v, double a, t_dcoord *out)
 	out->y = v.x * sin_a + v.y * cos_a;
 }
 
+/**
+ * function to set up the left and right rays for the player's 
+ * field of view (FOV). It calculates the direction of the left 
+ * and right rays by rotating the player's direction vector by
+ * half of the FOV angle in both directions. The left 
+ * ray is rotated by the negative half FOV angle, while
+ * the right ray is rotated by the positive half FOV angle. The
+ * function initializes both rays using the init_ray function,
+ * setting their angles based on the rotated direction vectors.
+ */
 static void	set_fov_rays(t_player *player, t_ray *left,
 			t_ray *right, t_data *target)
 {
